1504c9a1ab68e3992677ea5e72507eee8de53735
ajhuiux <84900918+ajhuiux@users.noreply.github.com>
2021-10-12 16:48:30 +0900

    수정

--- indexfile/js/index.js
+++ indexfile/js/index.js
@@ -1,57 +1,9 @@
-// var swiper1 = new Swiper('.horizon', {
-//     slidesPerView: "auto",
-//     freeMode: true,
-//     mousewheel: true,
-//     keyboard: {
-//         enabled: true,
-//         onlyInViewport: false,
-//     },
-// });
-
-// var swiper2 = new Swiper(".workSwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     loop: true,
-//     navigation: {
-//         nextEl: ".swiper-button-next.workBt",
-//         prevEl: ".swiper-button-prev.workBt",
-//     },
-// });
-
-
-var iw = window.innerWidth;
-if (iw > 641) {
-    // new Swiper('.horizon', {
-    //     slidesPerView: "auto",
-    //     direction: "vertical",
-    //     // freeMode: true,
-    //     mousewheel: true,
-    //     keyboard: {
-    //         enabled: true,
-    //         onlyInViewport: false,
-    //     },
-    // });
-
-    new Swiper(".workSwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        navigation: {
-            nextEl: ".swiper-button-next.workBt",
-            prevEl: ".swiper-button-prev.workBt",
-        },
-    });
-
-} else if (iw < 641) {
-    
-}
-
 $(function () {
     $("main").addClass("text-focus-in");
-    
+  
     $("a.expBt.ready").click(function() {
-        alert('사이트 준비중입니다 :)');
-        console.log('사이트 준비중입니다 :)');
+        alert('추후 업데이트 예정입니다. :)');
+        console.log('추후 업데이트 예정입니다. :)');
         return false;
     })
     //  모바일 전용 메뉴버튼
