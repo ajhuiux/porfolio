59bcc592ca2c4af7465df35afca9f7792e3bbc65
ajhuiux <84900918+ajhuiux@users.noreply.github.com>
2021-10-27 18:11:11 +0900

    skill_add

--- indexfile/js/pf-work.js
+++
@@ -1,9 +0,0 @@
-new Swiper(".workSwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next.workBt",
-        prevEl: ".swiper-button-prev.workBt",
-    },
-});
